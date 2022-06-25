bc91241b438267c46372c349a6b4d244d80eae4f
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-06-25 14:00:22 -0400

    Refocting 3

--- app/src/main/java/app/TimeMan/TimeManApplication.kt
+++ app/src/main/java/app/TimeMan/TimeManApplication.kt
@@ -6,14 +6,14 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext
 import org.koin.core.logger.Level
 
-class MyPlantDiaryApplication : Application() {
+class TimeManApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         GlobalContext.startKoin {
             androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
-            androidContext(this@MyPlantDiaryApplication)
+            androidContext(this@TimeManApplication)
             modules(appModule)
         }
     }
